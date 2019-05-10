f3616ca4413ae7f09763911a6aa48de8a7034092
Alex Lepsa <lepsalex@gmail.com>
2019-05-10 16:00:45 -0400

    Initial commit - enemy moves through course

---
+++ Assets/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+  public float speed = 10f;
+
+  private Transform target;
+  private int waypointIndex = 0;
+
+  void Start() {
+    target = Waypoints.points[0];
+  }
+
+  void Update() {
+    Vector3 direction = target.position - transform.position;
+    transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+    if (Vector3.Distance(transform.position, target.position) <= 0.2f) {
+      GetNextWaypoint();
+    }
+  }
+
+  void GetNextWaypoint() {
+    if (waypointIndex >= Waypoints.points.Length - 1) {
+      Destroy(gameObject);
+      return;
+    }
+
+    waypointIndex++;
+    target = Waypoints.points[waypointIndex];
+  }
+}
---
+++ Assets/Waypoints.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class Waypoints : MonoBehaviour {
+  public static Transform[] points;
+
+  void Awake() {
+    points = Enumerable.Range(0, transform.childCount)
+                .Select(x => transform.GetChild(x))
+                .ToArray();
+  }
+}
